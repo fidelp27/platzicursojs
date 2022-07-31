5544fd78fa4e9a12b6b0dab9e1f81263079b1793
fidelp27 <fidel2702@gmail.com>
2022-07-31 08:47:31 -0300

    Updates

---
+++ data.js
@@ -0,0 +1,94 @@
+const products = [
+  {
+    name: 'Bike',
+    price: '140',
+    image:
+      'https://images.pexels.com/photos/1595476/pexels-photo-1595476.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    name: 'Cart',
+    price: '160',
+    image:
+      'https://images.pexels.com/photos/1595476/pexels-photo-1595476.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    name: 'Motocycle',
+    price: '200',
+    image:
+      'https://images.pexels.com/photos/1595476/pexels-photo-1595476.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    name: 'Rollerblade',
+    price: '180',
+    image:
+      'https://images.pexels.com/photos/1595476/pexels-photo-1595476.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    name: 'Rollerblade Xtreme',
+    price: '300',
+    image:
+      'https://images.pexels.com/photos/1595476/pexels-photo-1595476.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    name: 'Bike',
+    price: '140',
+    image:
+      'https://images.pexels.com/photos/1595476/pexels-photo-1595476.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    name: 'Cart',
+    price: '160',
+    image:
+      'https://images.pexels.com/photos/1595476/pexels-photo-1595476.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    name: 'Motocycle',
+    price: '200',
+    image:
+      'https://images.pexels.com/photos/1595476/pexels-photo-1595476.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    name: 'Rollerblade',
+    price: '180',
+    image:
+      'https://images.pexels.com/photos/1595476/pexels-photo-1595476.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    name: 'Rollerblade Xtreme',
+    price: '300',
+    image:
+      'https://images.pexels.com/photos/1595476/pexels-photo-1595476.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    name: 'Bike',
+    price: '140',
+    image:
+      'https://images.pexels.com/photos/1595476/pexels-photo-1595476.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    name: 'Cart',
+    price: '160',
+    image:
+      'https://images.pexels.com/photos/1595476/pexels-photo-1595476.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    name: 'Motocycle',
+    price: '200',
+    image:
+      'https://images.pexels.com/photos/1595476/pexels-photo-1595476.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    name: 'Rollerblade',
+    price: '180',
+    image:
+      'https://images.pexels.com/photos/1595476/pexels-photo-1595476.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    name: 'Rollerblade Xtreme',
+    price: '300',
+    image:
+      'https://images.pexels.com/photos/1595476/pexels-photo-1595476.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+];
+
+export default products;
